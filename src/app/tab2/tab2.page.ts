8bdfc08a58e0e5a62b52b5db7ea8615fa9be37a4
fbaki12 <fbaki@futurion.tech>
2025-01-31 11:55:02 +0000

    added appflow sdk

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,3 +1,5 @@
+import { ActionSheetController } from '@ionic/angular';
+import { PhotoService, UserPhoto } from './../services/photo.service';
 import { Component } from '@angular/core';
 
 @Component({
@@ -8,6 +10,38 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  constructor(public photoService: PhotoService,
+    public actionSheetController: ActionSheetController
+  ) {}
+
+  addPhotoToGallery(){
+    this.photoService.addNewToGallery();
+  }
+
+  async ngOnInit() {
+    await this.photoService.loadSaved();
+  }
+
+  public async showActionSheet(photo: UserPhoto, position: number) {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Photos',
+      buttons: [{
+        text: 'Delete',
+        role: 'destructive',
+        icon: 'trash',
+        handler: () => {
+          this.photoService.deletePicture(photo, position);
+        }
+      }, {
+        text: 'Cancel',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          // Nothing to do, action sheet is automatically closed
+          }
+      }]
+    });
+    await actionSheet.present();
+  }
 
 }
